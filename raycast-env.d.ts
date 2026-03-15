dfb92d0332d2738e634ed9b8c66c86fb02472a7d
Dheeraj <72682860+Drj0@users.noreply.github.com>
2026-03-15 18:45:44 +0530

    feat: Add clipboard support for images and text (#2)
    Co-authored-by: Dheeraj <dheerajm435@gmail.com>

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,9 +15,13 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `index` command */
   export type Index = ExtensionPreferences & {}
+  /** Preferences accessible in the `add-from-clipboard` command */
+  export type AddFromClipboard = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `index` command */
   export type Index = {}
+  /** Arguments passed to the `add-from-clipboard` command */
+  export type AddFromClipboard = {}
 }
